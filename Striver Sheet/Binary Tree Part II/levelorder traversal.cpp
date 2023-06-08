d30e8ad20ad7138b4da007e27d6e0941e8d8ad20
KESHAV MITTAL <75433550+mittal0802@users.noreply.github.com>
2023-06-08 12:41:01 +0530

    Add files via upload

---
+++ Striver Sheet/Binary Tree Part II/levelorder traversal.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>>ans;
+        if(!root)return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+           int sz=q.size();
+           vector<int>v;
+           while(sz--){
+               TreeNode * f=q.front();
+               v.push_back(q.front()->val);
+               q.pop();
+               if(f->left)q.push(f->left);
+               if(f->right)q.push(f->right);
+
+           }
+           ans.push_back(v);
+        }
+        return ans;
+    }
+};
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector <vector<int>> ans;
+//         if(!root)
+//         {
+//             return ans;
+//         }
+//         queue <pair<TreeNode*, int>> q;
+//         q.push({root, 0});
+//         while(!q.empty())
+//         {
+//             TreeNode *fr = q.front().first;
+//             int level = q.front().second;
+//             if(fr->left)
+//             {
+//                 q.push({fr->left, level+1});
+//             }
+//             if(fr->right)
+//             {
+//                q.push({fr->right, level+1});
+//             }
+//             if(level+1 > ans.size())
+//             {
+//                 ans.push_back({});
+//             }
+//             ans[level].push_back(fr->val);
+//             q.pop();
+//         } 
+//         return ans;
+//     }
+// };
